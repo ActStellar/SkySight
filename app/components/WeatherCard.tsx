1736c5bcfaadc42f773b0b2963050b2f96982d88
GOAT.S <i.ryu9150@gmail.com>
2024-12-11 17:57:21 +0900

    UI修正

--- app/components/WeatherCard.tsx
+++ app/components/WeatherCard.tsx
@@ -1,7 +1,41 @@
 import { formatTemperature, formatWeatherDescription } from '../utils/formatWeatherData';
+import { TiWeatherCloudy, TiWeatherSunny, TiWeatherDownpour, TiWeatherShower, TiWeatherStormy, TiWeatherSnow, TiWeatherWindy } from "react-icons/ti";
 
 interface WeatherCardProps {
   weather: any;
+}
+
+const WeatherIconFormat = (weather: string) => {
+  switch (weather) {
+    case 'Clear sky':
+      return <TiWeatherSunny style={{fontSize: '200px'}}/>
+    case 'Few clouds':
+    case 'Scattered clouds':
+    case 'Broken clouds':
+      return <TiWeatherCloudy style={{fontSize: '200px'}}/>
+    case 'Overcast clouds':
+      return <TiWeatherCloudy style={{color: 'gray', fontSize: '200px'}}/>
+    case 'Shower rain':
+      return <TiWeatherShower style={{fontSize: '200px'}}/>
+    case 'Rain':
+      return <TiWeatherDownpour style={{fontSize: '200px'}}/>
+    case 'Thunderstorm':
+      return <TiWeatherStormy style={{fontSize: '200px'}}/>
+    case 'Snow':
+      return <TiWeatherSnow style={{fontSize: '200px'}}/>
+    case 'Mist':
+    case 'Smoke':
+    case 'Haze':
+    case 'Dust':
+    case 'Fog':
+    case 'Sand':
+    case 'Ash':
+    case 'Squall':
+    case 'Tornado':
+      return <TiWeatherWindy style={{fontSize: '200px'}}/>
+    default:
+      return <></>
+  }
 }
 
 const WeatherCard = ({ weather }: WeatherCardProps) => {
@@ -10,6 +44,7 @@ const WeatherCard = ({ weather }: WeatherCardProps) => {
   return (
     <div className="weather-card">
       <h2>{weather.name}</h2>
+      {WeatherIconFormat(weather.weather[0].description)}
       <p>{formatWeatherDescription(weather.weather[0].description)}</p>
       <p>{formatTemperature(weather.main.temp)}</p>
       <p>Humidity: {weather.main.humidity}%</p>
